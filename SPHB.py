3fed2a009bcd721dd13d188500fffd424739214e
Almas <62244354+almasfiza@users.noreply.github.com>
2021-09-13 22:28:13 +0530

    added the groups trends

--- SPHB.py
+++ SPHB.py
@@ -1,9 +1,32 @@
+from altair.vegalite.v4.schema.channels import Tooltip
+from altair.vegalite.v4.schema.core import TooltipContent
+from seaborn.palettes import color_palette
 import streamlit as st
 import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from pywaffle import Waffle
+import plotly.graph_objects as go
+from plotly import tools
+import plotly.offline as py
+import plotly.express as px
+import mpld3
+import webbrowser
+
+
+st.set_page_config(page_title = "Sleep patterns and human behaviour")
+st.sidebar.title("Sleep Patterns and Human Behaviour")
+st.sidebar.write("This is an EDA based on a survey conducted to determine the relationships and insights between sleeping patterns and human behaviour.")
+st.sidebar.write("***")
+st.sidebar.write("Say Hi to the author!")
+if st.sidebar.button("LinkedIn"):
+    webbrowser.open_new_tab("https://www.linkedin.com/in/almasfiza-shaikh-004048185/")
+if st.sidebar.button("Github"):
+    webbrowser.open_new_tab("https://github.com/almasfiza")
+
+
+
 
 
 
@@ -34,8 +57,9 @@ wafflefig = plt.figure(FigureClass = Waffle,
                 columns = 11,
                 values = pd.array(df["Your age group:"].value_counts()),
                 labels = ['18-25', '<17', '>25'],
-                colors = ["#C0C0C0","#DCDCDC","#000000"])
+                colors = ["#FF3EA5FF","#EDFF00FF","#00A4CCFF"])
 st.pyplot()
+st.write("The majority of participants are between the age of 18-25 years old, or classified as Young Adults.")
 
 
 
@@ -44,17 +68,63 @@ st.pyplot()
 st.write("***")
 st.write("""## WHEN DO PEOPLE GO TO SLEEP? """)
 st.set_option('deprecation.showPyplotGlobalUse', False)
-sns.countplot(y = df["What time do you usually go to sleep?"], data = df, color="#808495")
+sns.countplot(y = df["What time do you usually go to sleep?"], data = df, color="#EDFF00FF")
 st.pyplot()
 st.write(""" We can see that majority of the people in the study go to sleep around midnight. 
 
 """)
-st.write(df["What time do you usually go to sleep?"].groupby(df["Your age group:"]))
+
 st.write("***")
 st.write("""## WHEN DO PEOPLE WAKE UP? """)
 st.set_option('deprecation.showPyplotGlobalUse', False)
-sns.countplot(y = df["What time do you usually wake up?"], data = df, color="#808495")
+sns.countplot(y = df["What time do you usually wake up?"], data = df, color="#FF3EA5FF")
 st.pyplot()
+
+st.write("***")
+
+#df.plot(kind='bar', stacked=True)
+#plt.title("Age group and sleep time")
+#plt.xlabel("What time do you usually go to sleep?")
+#plt.ylabel("Do you wish you had more hours of sleep?")
+#st.pyplot()
+
+st.write("## GENERAL RESPONSES")
+
+
+option = st.selectbox(
+'See what majority of the people are doing',
+(df.columns[1:]))
+
+
+ax = sns.countplot(y = df[option], data = df, color="#00A4CCFF")
+plt.show()
+st.pyplot()
+
+
+
+
+st.write("## GROUPED TRENDS")
+
+
+
+
+
+option_group = st.selectbox(
+'Select a question',
+(df.columns[1:]))
+
+
+group = (df.drop(option_group,axis=1)).columns[1:]
+group_choose = st.radio("Group by ", group)
+
+
+
+
+
+df_plot = df.groupby([group_choose, option_group]).size().reset_index().pivot(columns=group_choose,index=option_group, values=0)
+df_plot.plot(kind='bar', stacked=True, color= ["#FF3EA5FF","#EDFF00FF","#00A4CCFF","#0C6291", "#EF476F"])
+st.pyplot()
+
 
 
 
